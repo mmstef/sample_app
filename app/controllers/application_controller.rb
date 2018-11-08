87493f467adb99be93984e44c0591fc32b5a7dae
magnus stef <maggistef@gmail.com>
2018-11-08 15:22:55 +0000

    Added log-in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
 
-  def hello
-    render html: "Hello World"
-  end
-
+  protect_from_forgery with: :exception
+  include SessionsHelper
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,31 @@
 class UsersController < ApplicationController
+  
+  def index
+    render html: "OK"
+  end
+  
   def new
     @user = User.new
   end
   
   def show
-  @user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
   
   def create
-  @user = User.new(user_params)
-  if @user.save
-    flash[:success] = "Til hamingju með daginn að vera kominn inn á síðuna."
-    redirect_to @user
-  else
-    render 'new'
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "Til hamingju með daginn að vera kominn inn á síðuna."
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
-end
 
 private
-
   def user_params
     params.require(:user).permit(:name, :email, :password, 
                                  :password_confirmation)
-  
   end
 end
