379394712a3feecde420e4dc3bacd92988e54563
magnus stef <maggistef@gmail.com>
2018-11-20 15:50:34 +0000

    FIX: Typos!

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -14,7 +14,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     patch user_path(@user), params: {user: { name: "",
                                   email: "user@invalid",
                                   password: "foo",
-                                  password_confirmation: "bar", bio: ""}}
+                                  password_confirmation: "bar"}}
     assert_template 'users/edit'
   end
   
